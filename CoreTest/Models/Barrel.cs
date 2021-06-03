83f1a5aa6bcd42a22681a2091b113ab0f2d00d0a
Mr O <ryan.p.osterberg@gmail.com>
2021-06-03 10:58:50 -0500

    Barrel class created

---
+++ CoreTest/Models/Barrel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreTest.Models
+{
+    public class Barrel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Radius { get; set; }
+        public double Height { get; set; }
+        public double Weight { get; set; }
+        public string ConstructionMaterial { get; set; }
+        public string Contents { get; set; }
+        public string CurrentLocation { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
